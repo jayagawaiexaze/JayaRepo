90ac20ffb9aa81ca0bfbe23f6ab601acbbd42779
Jaya Gawai <jaya.gawai@exaze.com>
2024-03-08 17:35:24 +0530

    Demo commit

---
+++ src/test/java/testcases/HelloProgram.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.junit.Test;
+
+public class HelloProgram {
+
+    @Test
+    public void printMessage() {
+        System.out.println("Hello World");
+    }
+}
+
+
+
+
