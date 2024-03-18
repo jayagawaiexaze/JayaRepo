1c7ab11423d15ed3f9821cd011c0b56782d7f14f
Jaya Gawai <jaya.gawai@exaze.com>
2024-03-18 18:16:51 +0530

    updated to use system property

--- src/test/java/testcases/HelloProgram.java
+++ src/test/java/testcases/HelloProgram.java
@@ -1,37 +1,14 @@
 package testcases;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.Test;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
 
 public class HelloProgram {
 
     @Test
     public void printMessage() {
         System.out.println("Hello World");
-        Dotenv dotenv = Dotenv.load();
-        String userName = dotenv.get("USERNAME1");//userName=jayag@mailinator.com
-
-        System.out.println(dotenv.get("USERNAME1"));
-        System.out.println(dotenv.get("PASSWORD1"));
-
-        Properties properties = new Properties();
-        try {
-            FileInputStream fileInputStream = new FileInputStream("src/test/resources/config.properties");
-            properties.load(fileInputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Get values from properties file
-        properties.setProperty("username", userName);
-
-        System.out.println(properties.getProperty("username"));
-
+        System.out.println(System.getProperty("username"));
+        System.out.println(System.getProperty("password"));
     }
 }
 
