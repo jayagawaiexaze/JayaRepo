66857af76c46ec5647205770308ea3a3babf3e28
Jaya Gawai <jaya.gawai@exaze.com>
2024-03-18 18:30:08 +0530

    updated to use system property

--- src/test/java/testcases/HelloProgram.java
+++ src/test/java/testcases/HelloProgram.java
@@ -7,8 +7,8 @@ public class HelloProgram {
     @Test
     public void printMessage() {
         System.out.println("Hello World");
-        System.out.println(System.getProperty("url"));
-        System.out.println(System.getProperty("username"));
+        System.out.println(System.getenv("url"));
+        System.out.println(System.getenv("username"));
         System.out.println(System.getProperty("password"));
     }
 }
