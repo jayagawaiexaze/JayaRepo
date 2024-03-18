509f80361e610d24b6b4bb188eff71b32762d883
Jaya Gawai <jaya.gawai@exaze.com>
2024-03-18 17:06:28 +0530

    code updated

--- src/test/java/testcases/HelloProgram.java
+++ src/test/java/testcases/HelloProgram.java
@@ -3,14 +3,35 @@ package testcases;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class HelloProgram {
 
     @Test
     public void printMessage() {
         System.out.println("Hello World");
         Dotenv dotenv = Dotenv.load();
-        System.out.println(dotenv.get("username"));
-        System.out.println(dotenv.get("password"));
+        String userName = dotenv.get("USERNAME1");//userName=jayag@mailinator.com
+
+        System.out.println(dotenv.get("USERNAME1"));
+        System.out.println(dotenv.get("PASSWORD1"));
+
+        Properties properties = new Properties();
+        try {
+            FileInputStream fileInputStream = new FileInputStream("src/test/resources/config.properties");
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Get values from properties file
+        properties.setProperty("username", userName);
+
+        System.out.println(properties.getProperty("username"));
+
     }
 }
 
