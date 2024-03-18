962a46364ff78cdf18fbb6724a5cb5f6683ec787
Jaya Gawai <jaya.gawai@exaze.com>
2024-03-18 15:17:19 +0530

    .env file added

--- src/test/java/testcases/HelloProgram.java
+++ src/test/java/testcases/HelloProgram.java
@@ -1,5 +1,6 @@
 package testcases;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.Test;
 
 public class HelloProgram {
@@ -7,6 +8,9 @@ public class HelloProgram {
     @Test
     public void printMessage() {
         System.out.println("Hello World");
+        Dotenv dotenv = Dotenv.load();
+        System.out.println(dotenv.get("username"));
+        System.out.println(dotenv.get("password"));
     }
 }
 
