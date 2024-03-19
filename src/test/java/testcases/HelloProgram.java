1f4f6b133e0614c558cb8a3941459adf12a9f2ba
Jaya Gawai <jaya.gawai@exaze.com>
2024-03-19 11:22:37 +0530

    updated code

--- src/test/java/testcases/HelloProgram.java
+++ src/test/java/testcases/HelloProgram.java
@@ -7,8 +7,9 @@ public class HelloProgram {
     @Test
     public void printMessage() {
         System.out.println("Hello World");
-        System.out.println(System.getenv("url"));
-        System.out.println(System.getenv("username"));
+       // System.out.println(System.getenv("url"));
+        //System.out.println(System.getenv("username"));
+        System.out.println(System.getProperty("username"));
         System.out.println(System.getProperty("password"));
     }
 }
